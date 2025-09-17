dc0926ec1483053d8f75a152141e4e98e9a38dbe
zico-jnr <okunadeqoyyum@gmail.com>
2025-09-17 04:55:50 -0700

    Update rola-luxe.js

--- rola-luxe.js
+++ rola-luxe.js
@@ -4,3 +4,12 @@ module.exports = {
     autoprefixer: {},
   },
 }
+
+@echo off
+echo Opening your Rola Luxe project folder...
+explorer "C:\Users\Fathia Okunade\ROLA RUXE"
+echo.
+echo Your project files are now open in File Explorer.
+echo Select all files (Ctrl+A) and copy them (Ctrl+C)
+echo Then go to GitHub and paste them (Ctrl+V)
+pause
